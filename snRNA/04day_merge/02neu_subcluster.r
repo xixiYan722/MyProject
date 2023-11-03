0c1c25cdc6e94b060f03d76e2007f6ede02e4e00
xixiYan722 <148738128+xixiYan722@users.noreply.github.com>
2023-11-03 18:13:21 +0800

    Update 02neu_subcluster.r

--- snRNA/04day_merge/02neu_subcluster.r
+++ snRNA/04day_merge/02neu_subcluster.r
@@ -16,7 +16,8 @@ neu <- NormalizeData(neu, normalization.method = "LogNormalize", scale.factor = 
 neu <- FindVariableFeatures(neu, selection.method = "vst", nfeatures = 2000)
 all.genes <- rownames(neu)
 neu <- ScaleData(neu, features = all.genes)
-neu <- RunPCA(neu, features = VariableFeatures(object = neu))
+
+neu <- RunPCA(neu, features = var2)
 ElbowPlot(neu,ndims=50)
 neu <- RunHarmony(neu, group.by.vars = "orig.ident", reduction = "pca",dims.use = 1:20, assay.use = "RNA",plot_convergence = TRUE)
 neu <- RunUMAP(neu, dims = 1:20, reduction = "harmony", reduction.name = "umap_harmony")
@@ -25,7 +26,7 @@ neu <- FindClusters(neu, resolution = seq(from =0.1,to = 1.0,by=0.1))
 
 # annotation
 clustree(neu)
-Idents(neu) <- "RNA_snn_res.0.2"
+Idents(neu) <- "RNA_snn_res.0.05"
 neu_markers <- FindAllMarkers(neu, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 neumarker<- neu_markers %>% group_by(cluster) %>% top_n(n = 2, wt = avg_log2FC)
 write.csv(neumarker,file="neu_markers.csv")
@@ -38,10 +39,10 @@ features = c("MKI67","NGFR","SOX10", ##neural crest stem cells marker
 DotPlot(neu, cols = c('white','#000080'),features = unique(features)) + RotatedAxis()
 
 levels(neu)
-new.cluster.ids=c('sub_NEU1','sub_NEU2','sub_NEU1','sub_NEU2','Neural_crest')
+new.cluster.ids=c('sub_NEU1','sub_NEU2','Neural_crest')
 names(new.cluster.ids) <- levels(neu)
 neu <- RenameIdents(neu, new.cluster.ids)
-neu$neusub_celltype <- Idents(neu)
+neu$neu_celltype <- Idents(neu)
 
 # save image and rds
 saveRDS(neu, "neu.rds")
